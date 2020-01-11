c8ec5d798c1d034d056b0706348ebeba199ed446
beillegalbeagle <you@example.com>
2020-01-11 09:12:49 +0000

    attempt host name fix

--- lib/mito_node/client.ex
+++ lib/mito_node/client.ex
@@ -3,7 +3,7 @@ defmodule MitoNode.Client do
 
   def start_link(state) do
     IO.inspect "I wanna be your girlll"
-    WebSockex.start_link("localhost:7076", __MODULE__, state)
+    WebSockex.start_link("ws://[::1]:7078", __MODULE__, state)
   end
 
   def handle_connect(conn, state) do
