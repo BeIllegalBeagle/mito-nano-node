40b5b509feb26807630f9b54c58786a1c1205fbb
beillegalbeagle <you@example.com>
2020-01-11 10:11:11 +0000

    attempt host name fix

--- lib/mito_node/client.ex
+++ lib/mito_node/client.ex
@@ -3,21 +3,22 @@ defmodule MitoNode.Client do
 
   def start_link(state) do
     IO.inspect "I wanna be your girlll"
-    tcp_conn = %WebSockex.Conn{
-      host: "localhost",
-      port: 7078,
-      path: "",
-      query: nil,
-      conn_mod: :gen_tcp,
-      transport: :tcp,
-      extra_headers: [{"Pineapple", "Cake"}],
-      socket: nil,
-      socket_connect_timeout: 6000,
-      socket_recv_timeout: 5000
-    }
-    IO.inspect tcp_conn
-    IO.inspect "I wanna rock your worldddd"
-    # WebSockex.start_link("ws://localhost:7078", __MODULE__, state)
+    # tcp_conn = %WebSockex.Conn{
+    #   host: "localhost",
+    #   port: 7078,
+    #   path: "",
+    #   query: nil,
+    #   conn_mod: :gen_tcp,
+    #   transport: :tcp,
+    #   extra_headers: [{"Pineapple", "Cake"}],
+    #   socket: nil,
+    #   socket_connect_timeout: 6000,
+    #   socket_recv_timeout: 5000
+    # }
+    # IO.inspect tcp_conn
+    # IO.inspect "I wanna rock your worldddd"
+    IO.inspect(state)
+    WebSockex.start_link("ws://localhost:7078", __MODULE__, state)
   end
 
   def handle_connect(conn, state) do
