ec0c6f101a75157806c98770da16b890a19d9eac
beillegalbeagle <you@example.com>
2020-01-12 09:15:28 +0000

    dont stop

--- lib/mito_node/client.ex
+++ lib/mito_node/client.ex
@@ -5,21 +5,26 @@ defmodule MitoNode.Client do
     WebSockex.start_link("ws://localhost:7078", __MODULE__, state)
   end
 
+  def init(parent, name, conn, module, module_state, opts) do
+
+    {:ok, parent}
+  end
+
   def handle_connect(conn, state) do
     IO.inspect("connected to the node!!!")
 
-  #   {:ok, message} = %{
-  #   "action": "subscribe",
-  #   "topic": "confirmation",
-  #   "options": %{
-  #     "all_local_accounts": true,
-  #     "accounts": [
-  #       "nano_1qzfp3op48im348qdybmrheu9dogtopj1jyioguq9pyo5i7mkqgo4jaswp4a"
-  #     ]
-  #   }
-  # } |> Jason.encode
-  #
-  #   WebSockex.cast(__MODULE__, message)
+    {:ok, message} = %{
+    "action": "subscribe",
+    "topic": "confirmation",
+    "options": %{
+      "all_local_accounts": true,
+      "accounts": [
+        "nano_1qzfp3op48im348qdybmrheu9dogtopj1jyioguq9pyo5i7mkqgo4jaswp4a"
+      ]
+    }
+  } |> Jason.encode
+  
+    WebSockex.cast(__MODULE__, message)
   end
 
   def handle_cast(msg, state) do
